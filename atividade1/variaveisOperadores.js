d1cc5514ba54f5ae32ce85c7663a82072969ae9d
MATHEUS HENRIQUE BOMFIM DE SOUZA <matheus.bomfim@a.ucb.br>
2023-08-10 10:46:13 -0300

    atividade1

--- atividade1/variaveisOperadores.js
+++ atividade1/variaveisOperadores.js
@@ -13,7 +13,7 @@ subtracao = num1 - num2;
 multiplicacao = num1 * num2;
 divisao = num1 / num2;
 
-console.log ('Resultado da soma entre ' + num1 + ' e ' + num2 + ' é ' + soma);
-console.log ('Resultado da subtração entre ' + num1 + ' e ' + num2 + ' é ' + subtracao);
-console.log ('Resultado da multiplicação entre ' + num1 + ' e ' + num2 + ' é ' + multiplicacao);
-console.log ('Resultado da divisão entre ' + num1 + ' e ' + num2 + ' é ' + divisao);
+console.log (`Resultado da soma entre ${num1} e ${num2} é ${soma}`);
+console.log (`Resultado da subtração entre ${num1} e ${num2} é ${subtracao}`);
+console.log (`Resultado da multiplicação entre ${num1} e ${num2} é ${multiplicacao}`);
+console.log (`Resultado da divisão entre ${num1} e ${num2} é ${divisao}`);
