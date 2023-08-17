ca23f5e58aad6bef3e91119a9eec6ac89cb9bc3e
MATHEUS HENRIQUE BOMFIM DE SOUZA <matheus.bomfim@a.ucb.br>
2023-08-17 11:01:37 -0300

    atividade2

---
+++ atividade2/exercicio1.js
@@ -0,0 +1,23 @@
+for (var i = 1; i <= 1; i++){
+    array = [
+        [1, 2],
+    ];
+
+    console.log(array);
+}
+
+for (var x = 1; x <= 1; x++){
+    array = [
+        [3, 4],
+    ];
+
+    console.log(array);
+}
+
+for (var j = 1; j <= 1; j++){
+    array = [
+        [5, 6],
+    ];
+
+    console.log(array);
+}
