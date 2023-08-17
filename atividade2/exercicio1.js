ea4fe92bb9faf04de425aef70ba0ba7d4bd454fd
Lucas Gabriel Neves Pereira <lucasnevespereira@hotmail.com>
2023-08-17 12:41:49 -0300

    dadaa

--- atividade2/exercicio1.js
+++ atividade2/exercicio1.js
@@ -1,6 +1,8 @@
+// CADA FOR É UMA LINHA E CADA VALOR DA ARRAY É UMA COLUNA
+
 for (var i = 1; i <= 1; i++){
     array = [
-        [1, 2],
+        1, 2,
     ];
 
     console.log(array);
@@ -8,7 +10,7 @@ for (var i = 1; i <= 1; i++){
 
 for (var x = 1; x <= 1; x++){
     array = [
-        [3, 4],
+        3, 4,
     ];
 
     console.log(array);
@@ -16,7 +18,25 @@ for (var x = 1; x <= 1; x++){
 
 for (var j = 1; j <= 1; j++){
     array = [
-        [5, 6],
+        5, 6,
+    ];
+
+    console.log(array);
+} 
+
+console.log();
+
+for (var i = 1; i <= 1; i++){
+    array = [
+        1, 3, 5,
+    ];
+
+    console.log(array);
+}
+
+for (var i = 1; i <= 1; i++){
+    array = [
+        2, 4, 6,
     ];
 
     console.log(array);
---
+++ atividade2/exercicio2.js
@@ -0,0 +1,64 @@
+console.log();
+
+//Matriz A
+console.log('Matriz A');
+for (var a= 1; a <= 1; a++){
+    array = [
+        1, 2];
+
+    console.log(array);
+}
+
+for (a = 1; a <= 1; a++){
+    array = [
+        3, 4];
+
+    console.log(array);
+}
+
+console.log();
+
+//Matriz B 
+console.log('Matriz B');
+for (var b = 1; b <= 1; b++){
+    array = [
+        7, 3];
+
+    console.log(array);
+}
+
+for (b = 1; b <= 1; b++){
+    array = [
+        9, 2];
+
+    console.log(array);
+}
+
+console.log();
+
+//Matriz C
+console.log('A multiplicação entre as matrizes A e B é');
+for (var c = 1; c <= 1; c++){
+    
+    var c11 = 1*7 + 2*9;
+    var c12 = 1*3 + 2*2;
+
+    array = [
+        c11, c12];
+
+
+    console.log(array);
+}
+
+for (c = 1; c <= 1; c++){
+    
+    var c21 = 3*7 + 4*9;
+    var c22 = 3*3 + 4*2;
+
+    array = [
+        c21, c22]; 
+
+    console.log(array);
+}
+
+console.log();
