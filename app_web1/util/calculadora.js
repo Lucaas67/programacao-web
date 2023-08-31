bf12e7c31d07f9f62d3f3a486a7f079148df4c63
MATHEUS HENRIQUE BOMFIM DE SOUZA <matheus.bomfim@a.ucb.br>
2023-08-31 10:39:17 -0300

    web

---
+++ app_web1/util/calculadora.js
@@ -0,0 +1,22 @@
+function somar(a,b){
+    return Number.parseInt(a) + Number.parseInt(b);
+}
+
+function subtrair(a,b){
+    return Number.parseInt(a) - Number.parseInt(b);
+}
+
+function multiplicar(a,b){
+    return Number.parseInt(a) * Number.parseInt(b);
+}
+
+function dividir(a,b){
+    return Number.parseInt(a) / Number.parseInt(b);
+}
+
+module.exports = {
+    somar,
+    subtrair,
+    multiplicar,
+    dividir
+}
