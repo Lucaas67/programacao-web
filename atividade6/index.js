76f5b89b89e0cf86bd9d9ae7ee56c3cd27462db6
Lucas Gabriel Neves Pereira <81247908+Lucaas67@users.noreply.github.com>
2023-10-04 18:47:03 -0300

    atividade6

--- atividade6/index.js
+++ atividade6/index.js
@@ -2,16 +2,16 @@ const express = require('express');
 const mustacheExpress = require('mustache-express');
 const app = express();
 
+app.get("/", function (req, res) {
+    res.send("Formulário");
+});
+
 app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
 app.get("/views/index.html", function(req, res){
-    res.render("index.html");
-});
-
-app.post("/views/", function (req, res) {   
-    res.render("index.html");
+    res.render("/views/index.html");
 });
 
 const PORT = 8080;
